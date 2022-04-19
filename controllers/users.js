943de799a5114d9b7043250d229ea7baf2e72f7e
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-19 08:50:45 +0200

    (minor) : add else to update user if userId does not match

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 
 const updateUser = async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     try {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
@@ -10,6 +10,8 @@ const updateUser = async (req, res) => {
     } catch (err) {
       return res.status(500).send(err);
     }
+  } else {
+    res.status(500).send("You can update only your account !");
   }
 };
 
