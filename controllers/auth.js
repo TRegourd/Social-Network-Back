97b3f0e73ca3076d31fb3a4f1a2d06117b2c2c23
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-24 15:04:51 +0200

    modified login and signin with bcrypt and asyn functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,36 +1,67 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { is } = require("express/lib/request");
+const saltRounds = 10;
 
-const createNewUser = (req, res) => {
+const createNewUser = async (req, res) => {
   const userForm = req.body;
 
-  const user = new User(userForm);
+  userForm.password = await bcrypt.hash(userForm.password, saltRounds);
 
-  User.create(user)
-    .then((result) => {
-      res.status(200);
-      res.send(`User created`);
-      console.log(`User created`);
-    })
-    .catch((error) => console.log(error));
+  const isUser = await User.find({ email: userForm.email });
+
+  if (isUser.length === 0) {
+    try {
+      const user = new User(userForm);
+      User.create(user).then(() => {
+        res.status(200);
+        console.log(`User created`);
+        res.send(`User created`);
+      });
+    } catch (error) {
+      console.log(error), res.senStatus(500);
+    }
+  } else {
+    res.status(400), console.log(`User already exists`);
+    res.send(`User already exists`);
+  }
 };
 
-const loginUser = (req, res) => {
-  let user;
+const loginUser = async (req, res) => {
+  if (!req.body.email || !req.body.password) return res.sendStatus(400);
 
-  User.findOne({ email: req.body.email })
-    .then((result) => {
-      if (!result) {
-        res.status(404).send("user not found");
-      } else {
-        user = result;
-        if (req.body.password != user.password) {
-          res.status(400).send("wrong password");
-        } else {
-          res.send(user);
-        }
-      }
-    })
-    .catch((error) => console.log(error));
+  let user = await User.findOne({ email: req.body.email });
+
+  if (user === null) {
+    res.status(400).send("No user found");
+  }
+
+  let matchingPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (matchingPassword) {
+    res.status(200).send("Successfully Login");
+  } else {
+    res.status(400).send("Incorrect Login");
+  }
+
+  //     .then((result) => {
+  //       if (!result) {
+  //         res.status(404).send("user not found");
+  //       } else {
+  //         user = result;
+
+  //         let matchingPassword = await bcrypt.compare(req.body.password, user.password);
+  // console.log(matchingPassword);
+
+  //         if (req.body.password != user.password) {
+  //           res.status(400).send("wrong password");
+  //         } else {
+  //           res.send(user);
+  //         }
+  //       }
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
 };
 
 const Auth = {
