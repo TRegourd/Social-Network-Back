d44fe5a9e5cf1fd01a66f13c26f87d1e410bef52
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-05 09:13:11 +0200

    created delete post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -25,9 +25,24 @@ const updatePost = async (req, res) => {
   }
 };
 
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne({ $set: req.body });
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 const Posts = {
   createPost,
   updatePost,
+  deletePost,
 };
 
 module.exports = Posts;
--- routes/posts.js
+++ routes/posts.js
@@ -8,4 +8,6 @@ router.post("/", Posts.createPost);
 
 router.put("/:id", Posts.updatePost);
 
+router.delete("/:id", Posts.deletePost);
+
 module.exports = router;
