dc867ddd19cbf53828f6f3b189a5b57aab1c8065
Thomas Regourd <tom.reg@hotmail.fr>
2022-05-05 09:14:17 +0200

    fixed bug

--- controllers/posts.js
+++ controllers/posts.js
@@ -29,7 +29,7 @@ const deletePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
-      await post.deleteOne({ $set: req.body });
+      await post.deleteOne();
       res.status(200).json("the post has been deleted");
     } else {
       res.status(403).json("you can delete only your post");
